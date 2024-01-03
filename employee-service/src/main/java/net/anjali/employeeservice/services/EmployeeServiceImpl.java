64874a2c9d33a9bb29553b58bd3d579039324751
anjaliasha123 <anjaliasha95@gmail.com>
2024-01-03 11:29:28 -0600

    web client spring WEBFLUX

--- employee-service/src/main/java/net/anjali/employeeservice/services/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/anjali/employeeservice/services/EmployeeServiceImpl.java
@@ -11,14 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService{
     @Autowired
     private EmployeeRepository employeeRepository;
+//    @Autowired
+//    private RestTemplate restTemplate;
     @Autowired
-    private RestTemplate restTemplate;
+    private WebClient webClient;
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
         Employee employee = EmployeeMapper.INSTANCE.employeeDtoToEmployee(employeeDto);
@@ -32,11 +35,16 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public APIResponseDto getEmployee(Long id) {
         Employee employee = this.employeeRepository.findById(id).get();
-        ResponseEntity<DepartmentDto> responseEntity =  restTemplate.getForEntity(
-                "http://localhost:8080/api/departments/department/"+employee.getDepartmentCode(),
-                DepartmentDto.class
-                );
-        DepartmentDto departmentDto = responseEntity.getBody();
+//        ResponseEntity<DepartmentDto> responseEntity =  restTemplate.getForEntity(
+//                "http://localhost:8080/api/departments/department/"+employee.getDepartmentCode(),
+//                DepartmentDto.class
+//                );
+//        DepartmentDto departmentDto = responseEntity.getBody();
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/department/"+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
         EmployeeDto employeeDto = EmployeeMapper.INSTANCE.employeeToEmployeeDto(employee);
 
         APIResponseDto apiResponseDto = new APIResponseDto();
