a1076f300983e01c86175282557848206f7eac4a
anjaliasha123 <anjaliasha95@gmail.com>
2024-01-23 15:03:17 -0600

    centralized config & auto refresh

---
+++ department-service/src/main/java/net/anjali/departmentservice/controllers/MessageController.java
@@ -0,0 +1,17 @@
+package net.anjali.departmentservice.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class MessageController {
+    @Value("${spring.boot.message}")
+    private String message;
+    @GetMapping("message")
+    public String message(){
+        return message;
+    }
+}
